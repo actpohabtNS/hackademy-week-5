a72915a3e57a7dbe65c06ba0dc09d776e6c91a9e
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-09-12 17:54:18 +0300

    [A] landing page

--- react-next-app/pages/_app.js
+++ react-next-app/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import '../styles/landing.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
